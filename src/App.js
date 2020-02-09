1149e4fc637e7e0a4e6ca96368cb4fb0af1ad1d0
hanachanm <aya.yb.0427@gmail.com>
2020-02-09 15:51:35 +0900

    jsxについて

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+// divタグの部分のコードがjsxと呼ばれるもの
+// jsxはjavascriptXMLの略
+// javascriptを拡張したテンプレート言語の一つ（reactで）使うために開発された
 function App() {
-  return <div><h1>Hello World!</h1></div>;
+  return <h1>Hello World!</h1>;
 }
+
+// jsで書くとこうなる
+// function App() {
+//   return React.createElement(
+//     "div",
+//     null,
+//     "helloworld"
+//   );
+// }
+
+
 
 export default App;
