bba6613de1bffb6b7f1ffb0c26319c60b067bb44
hanachanm <aya.yb.0427@gmail.com>
2020-02-09 16:08:41 +0900

    more jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+// jsxを使う際には、Reactの明記が必要
 
 function App() {
-  return <div><h1>Hello World!</h1></div>;
+  // const greeting = "Hi!"
+  // const dom = <h1 className="foo">{greeting}</h1>;
+  // return dom;
+
+  // return <input type="text" onClick={() => {console.log("I am clicked")}} />;
+
+  // return でかえすjsxは1つのタグでなければならない。-> divで囲う
+  // divで囲うと、Rectで使うためだけの余計なdiv要素が含まれてしまう。 -> divの代わりにReact.Fragment
+  return(
+    <React.Fragment>
+    {/* <div> */}
+      <label htmlFor="bar">bar</label>
+      <input type="text" onClick={() => {console.log("I am clicked")}} />
+    {/* </div> */}
+    </React.Fragment>
+  )
 }
 
 export default App;
